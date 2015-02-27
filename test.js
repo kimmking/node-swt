5259c123b83a57ccc0144b486424845258ce4fd9
kimmking <kimmking@163.com>
2015-02-27 17:17:53 +0800

    add control

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var ffi = require('ffi'),
   ref = require('ref'),
+  os = require('os'),
   Struct = require('ref-struct'),
   hWnd = new Buffer(8);
 
@@ -40,18 +41,26 @@ var user32 = ffi.Library('user32', {
   'GetMessageW': ['bool', ['pointer', 'pointer', 'uint32', 'uint32']],
   'TranslateMessageEx': ['bool', ['pointer']],
   'DispatchMessageW': ['uint32', ['pointer']],
-  'PostQuitMessage': ['void', ['int32']]
+  'PostQuitMessage': ['void', ['int32']],
+  'GetWindowLongW' : ['pointer', ['pointer','int32']],
+  'SendMessageW':['uint32', ['pointer','uint32','int32','pointer']]
 });
 
 var kernel32 = ffi.Library('kernel32', {
   'GetModuleHandleExW': ['bool', [
     'int32', 'pointer', 'pointer' // flags, optional LPCTSTR name, ref hModule
   ]], // etc... / kernel32 is your friend!
+  
+  'GetCurrentProcess': ['pointer', [
+  ]],
+  'IsWow64Process': ['bool', [
+    'pointer', 'pointer' 
+  ]]
 });
 var gdi32 = ffi.Library('gdi32', {
-  'CreateSolidBrush': ['pointer', [
-    'int32' 
-  ]],   
+  'CreateSolidBrush': ['pointer', [ 'int32' ]],  
+  'SetBkColor': ['int32', [ 'pointer','int32' ]],  
+  'SetTextColor': ['int32', [ 'pointer','int32' ]],  
 });
 
 var Msg = Struct({
@@ -63,6 +72,16 @@ var Msg = Struct({
   'pX': 'int32',
   'pY': 'int32'
 });
+
+// test os bit
+var cbSize = 48;
+var pthread = new Buffer(8);
+var bf = new Buffer(1);
+pthread = kernel32.GetCurrentProcess();
+console.log("pthread => "+pthread);
+var r = kernel32.IsWow64Process(pthread,bf);
+console.log("IsWow64Process => "+r + "," + bf[0]);
+if(r && !bf[0] && os.arch()=='x64') cbSize = 80;
 
 // App-specific
 
@@ -83,13 +102,23 @@ var WndProc = ffi.Callback('uint32',
     switch(uMsg) {
       case 2:
         user32.PostQuitMessage(0);
+        console.log("quit hWnd=> " + hwnd.length);
+        return 0;
+      case 0x0133: // WM_CTLCOLOREDIT
+        //gdi32.SetTextColor(wParam,0x0080ff);
+        //gdi32.SetBkColor  (wParam,0xffff00);
+        return 0;
+        // WM_CTLCOLORLISTBOX 134 WM_CTLCOLORBTN 135
+      case 0x0138: // WM_CTLCOLORSTATIC
+        //gdi32.SetTextColor(wParam,0xffff00);
+        //gdi32.SetBkColor  (wParam,0x0080ff);
         return 0;
       default:
         result = user32.DefWindowProcW(hwnd, uMsg, wParam, lParam);
-        console.log('DefWindowProc LRESULT: ' + result);
+        if(result) console.log('DefWindowProc LRESULT: ' + result );
         return result;
     }
-    console.log('Sending LRESULT: ' + result.deref())
+    //console.log('Sending LRESULT: ' + result.deref())
     return result;
   }
 );
@@ -102,7 +131,7 @@ comctl32.InitCommonControlsEx(icc.ref());
 
 // Window Class
 var wClass = new WndClassEx;
-wClass.cbSize = 48; //80; // x86 = 48
+wClass.cbSize = cbSize;//48; //80; // x86 = 48
 wClass.style = 3;
 wClass.lpfnWndProc = WndProc;
 wClass.cbClsExtra = 0;
@@ -110,7 +139,7 @@ wClass.cbWndExtra = 0;
 wClass.hInstance = hInstance;
 wClass.hIcon = null;
 wClass.hCursor = null;
-wClass.hbrBackground = gdi32.CreateSolidBrush(0xff8000);//null;
+wClass.hbrBackground = gdi32.CreateSolidBrush(0xf0800f);//null;
 wClass.lpszMenuName = null;
 wClass.lpszClassName = className;
 wClass.hIconSm = null;
@@ -130,10 +159,99 @@ hWnd = user32.CreateWindowExW(
   null, null, hInstance, null
 );
 
-console.log(hWnd);
+var btnClassName = new Buffer('BUTTON\0', 'ucs-2');
+var btnWindowName = new Buffer('Button\0', 'ucs-2');
+var hInst = user32.GetWindowLongW(hWnd, -6);
 
-user32.ShowWindow(hWnd, 1);
-user32.UpdateWindow(hWnd);
+var hWndButton = user32.CreateWindowExW( 
+  0,
+  btnClassName,   // Predefined class; Unicode assumed. 
+  btnWindowName,       // Button text. 
+  1342242817, //WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles. 
+  10,         // x position. 
+  10,         // y position. 
+  100,        // Button width.
+  30,        // Button height.
+  hWnd,       // Parent window.
+  null,       // No menu.
+  hInst,
+  null);      // Pointer not needed.
+
+var hWndText = user32.CreateWindowExW( 
+  0,
+  new Buffer('EDIT\0', 'ucs-2'),   // Predefined class; Unicode assumed. 
+  new Buffer('TextBox\0', 'ucs-2'),       // Button text. 
+  1342242817,  
+  130, 
+  10,  
+  100,
+  16,
+  hWnd,
+  null,
+  hInst,
+  null);
+
+var hWndStatic = user32.CreateWindowExW( 
+  0,
+  new Buffer('STATIC\0', 'ucs-2'),   // Predefined class; Unicode assumed. 
+  new Buffer('Label\0', 'ucs-2'),       // Button text. 
+  1342242817,  
+  10, 
+  70,  
+  100,
+  20,
+  hWnd,
+  null,
+  hInst,
+  null);
+
+var hWndComboBox = user32.CreateWindowExW( 
+  0,
+  new Buffer('COMBOBOX\0', 'ucs-2'),   // Predefined class; Unicode assumed. 
+  new Buffer('ComboBox\0', 'ucs-2'),       
+  1344274947,  
+  10, 
+  100,  
+  100,
+  30,
+  hWnd,
+  null,
+  hInst,
+  null);
+
+var hWndListBox = user32.CreateWindowExW( 
+  0,
+  new Buffer('LISTBOX\0', 'ucs-2'),   // Predefined class; Unicode assumed. 
+  new Buffer('ListBox1\0', 'ucs-2'),        
+  1342242817,  
+  150, 
+  50,  
+  100,
+  80,
+  hWnd,
+  null,
+  hInst,
+  null);
+
+
+console.log("handles=> "+hWnd+"[btn="+hWndButton+"]");
+
+var flag = user32.ShowWindow(hWnd, 1);
+//console.log("ShowWindow => " + flag);
+flag = user32.UpdateWindow(hWnd);
+//console.log("UpdateWindow => " + flag);
+
+// var someBuffer = new Buffer('whatever');
+// var buf = ref.alloc('pointer');
+// ref.writePointer(buf, 0, someBuffer);
+
+user32.SendMessageW(hWndComboBox,0x0143,0,new Buffer('大漠穷秋\0', 'ucs-2'));
+user32.SendMessageW(hWndComboBox,0x0143,0,new Buffer('太空飞猪\0', 'ucs-2'));
+user32.SendMessageW(hWndComboBox,0x014e,0,null);
+
+user32.SendMessageW(hWndListBox,0x0180,0,new Buffer('大漠穷秋\0', 'ucs-2'));
+user32.SendMessageW(hWndListBox,0x0180,0,new Buffer('太空飞猪\0', 'ucs-2'));
+user32.SendMessageW(hWndListBox,0x0186,1,null);
 
 // message loop
 var msg = new Msg;
